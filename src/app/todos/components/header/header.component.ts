f0fed137c729dfb60ce219819e7fb971cdd2e785
MTeixeira9 <mario_ze16@hotmail.com>
2022-03-22 17:30:15 +0000

    Get todo text

---
+++ src/app/todos/components/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todos-header',
+  templateUrl: './header.component.html',
+})
+export class HeaderComponent {
+  text: string = '';
+
+  changeText(event: Event) {
+    const target = event.target as HTMLInputElement;
+    this.text = target.value;
+  }
+
+  addTodo(): void {}
+}
